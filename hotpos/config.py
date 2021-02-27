2b1ac10d315b5f052a23c1ab8759d4c2a7f44a8b
Hotte Shen <hotteshen@gmail.com>
2021-02-26 19:46:07 -0600

    set window icon, title.

--- hotpos/config.py
+++ hotpos/config.py
@@ -1,2 +1,10 @@
-APP_NAME = 'hotpos'
+from pathlib import Path
+
+
+APP_NAME = 'HotPOS'
 VERSION = 1
+
+ROOT_PATH = Path(__file__).parent.parent
+RES_PATH = ROOT_PATH / 'res'
+
+WINDOW_MIN_SIZE = (800, 600)
--- hotpos/widgets/main_window.py
+++ hotpos/widgets/main_window.py
@@ -1,12 +1,20 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QWidget, QVBoxLayout
 
 from .login_widget import LoginWidget
+from ..config import WINDOW_MIN_SIZE, APP_NAME, RES_PATH
 
 
 class MainWindow(QWidget):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.setWindowTitle(APP_NAME)
+        self.setWindowFlags( Qt.CustomizeWindowHint | Qt.WindowCloseButtonHint )
+        self.setWindowIcon(QIcon(str(RES_PATH / 'icon.png')))
+        self.setMinimumSize(*WINDOW_MIN_SIZE)
 
         root_layout = QVBoxLayout()
         self.setLayout(root_layout)
