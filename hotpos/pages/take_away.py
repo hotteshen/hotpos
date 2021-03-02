94992d221bc5d7737ff736b8702488ca52a0050d
Hotte Shen <hotteshen@gmail.com>
2021-03-02 14:46:16 -0600

    sow icons on main category list.

--- hotpos/pages/take_away.py
+++ hotpos/pages/take_away.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QHBoxLayout, QWidget, QLabel, QListWidget, QListWidgetItem
 
 from ..widgets.group_box import GroupBoxWidget
+from ..widgets.main_category_list import MainCategoryListWidget, MainCategory
 
 
 class TakeAwayPage(QWidget):
@@ -11,7 +12,7 @@ class TakeAwayPage(QWidget):
         root_layout = QHBoxLayout()
         self.setLayout(root_layout)
 
-        self.main_category_list_widget = QListWidget()
+        self.main_category_list_widget = MainCategoryListWidget()
         root_layout.addWidget(self.main_category_list_widget, 0)
 
         gb = GroupBoxWidget()
@@ -30,7 +31,7 @@ class TakeAwayPage(QWidget):
         self.category_data = [
             {
                 'name': 'Food',
-                'image': None,
+                'image': 'main-category-food.png',
                 'sub_category_list': [
                     {
                         'name': 'All',
@@ -44,7 +45,7 @@ class TakeAwayPage(QWidget):
             },
             {
                 'name': 'Drinks',
-                'image': None,
+                'image': 'main-category-drink.png',
                 'sub_categorie_list': [],
             },
         ]
@@ -53,8 +54,8 @@ class TakeAwayPage(QWidget):
     def showMainCategory(self, main_cat=-1):
         self.main_category_list_widget.clear()
         for cat in self.category_data:
-            list_item = QListWidgetItem(cat['name'])
-            self.main_category_list_widget.addItem(list_item)
+            cat_obj = MainCategory(cat['name'], cat['image'])
+            self.main_category_list_widget.addItem(cat_obj)
             if main_cat >= 0:
                 self.showSubCategory(main_cat=main_cat, sub_cat=0)
 
---
+++ hotpos/widgets/main_category_list.py
@@ -0,0 +1,63 @@
+from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtWidgets import QVBoxLayout, QWidget, QListWidget, QListWidgetItem, QLabel, QSizePolicy
+from PyQt5.QtGui import QPixmap
+
+from ..config import RES_PATH, MAIN_CAT_ICON_SIZE, MAIN_CAT_LIST_WIDTH
+from .label import LabelWidget
+
+
+class MainCategory:
+
+    def __init__(self, name: str, image: str):
+        self.name = name
+        self.image = image
+
+
+class MainCategoryItemWidget(QWidget):
+    def __init__(self, item: MainCategory, parent=None):
+        super(MainCategoryItemWidget, self).__init__(parent)
+
+        root_layout = QVBoxLayout(self)
+
+        image_label = QLabel()
+        image_label.setAlignment(Qt.AlignCenter)
+        image_map = QPixmap(str(RES_PATH / 'category_images' / item.image))
+        image_map = image_map.scaled(QSize(*MAIN_CAT_ICON_SIZE), Qt.KeepAspectRatio)
+        image_label.setPixmap(image_map)
+        root_layout.addWidget(image_label)
+
+        name = LabelWidget(item.name).setCenter()
+        name.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        root_layout.addWidget(name)
+
+
+class _ListWidget(QListWidget):
+
+  def sizeHint(self):
+    s = QSize()
+    s.setHeight(super(_ListWidget, self).sizeHint().height())
+    s.setWidth(MAIN_CAT_LIST_WIDTH)
+    return s
+
+
+class MainCategoryListWidget(QWidget):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        root_layout = QVBoxLayout(self)
+        self.list_view = _ListWidget()
+        root_layout.addWidget(self.list_view)
+
+    def addItem(self, item: MainCategory):
+        item_widget = QListWidgetItem(self.list_view)
+        self.list_view.addItem(item_widget)
+        custom_item_widget = MainCategoryItemWidget(item)
+        item_widget.setSizeHint(custom_item_widget.sizeHint())
+        self.list_view.setItemWidget(item_widget, custom_item_widget)
+
+    def setCurrentRow(self, row: int):
+        self.list_view.setCurrentRow(row)
+
+    def clear(self):
+        self.list_view.clear()
